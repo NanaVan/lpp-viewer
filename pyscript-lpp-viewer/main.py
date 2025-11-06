0ea1ce3fc375a5273e9d676219afba5d887ebd1a
NanaVan <van4wang@hotmail.com>
2025-11-06 15:54:49 +0800

    fix typo for the online display

--- pyscript-lpp-viewer/main.py
+++ pyscript-lpp-viewer/main.py
@@ -24,25 +24,25 @@ async def process_file(event):
                 markdown_words.object = '## Upload and parse your .lpp file'
                 pn.pane.Alert('## Alert\n### Some error may exist in your .lpp file. Please check your file first.\nFormat of .lpp is encoded as utf-8, please change to CP932', alert_type='danger').servable(target='testShow')
                 return
-            #try:
-            console.log('Bokeh: loading...')
-            if len(checkbutton_display.value) == 2:
-                show_Bokeh = Bokeh_show('new.lpp', 308., 3000, 4096, 0.1, 1.43, 0.2, 1.36, 0.5, 0.5)._show('Both')
-            elif len(checkbutton_display.value) == 1 and 'TOF' in checkbutton_display.value:
-                show_Bokeh = Bokeh_show('new.lpp', 308., 3000, 4096, 0.1, 1.43, 0.2, 1.36, 0.5, 0.5)._show('TOF')
-            elif len(checkbutton_display.value) == 1 and 'Schottky' in checkbutton_display.value:
-                show_Bokeh = Bokeh_show('new.lpp', 308., 3000, 4096, 0.1, 1.43, 0.2, 1.36, 0.5, 0.5)._show('Schottky')
-            else:
-                pass
-            console.log('Bokeh: complete')
-            pn.pane.Alert('## Info\n### The current interface is for functional testing only. \n### Please wait for the final version.').servable(target='testShow')
-            pn.pane.Bokeh(show_Bokeh).servable(target='fileShow')
-            markdown_words.object = '## Upload and parse your .lpp file'
-            button_upload.disabled = False
-            #except:
-            #    console.log('.lpp file error!')
-            #    markdown_words.object = '## Upload and parse your .lpp file'
-            #    pn.pane.Alert('## Alert\n### Some error exist when parsing your .lpp file.\nYou can send your .lpp file to <u>wangqian2016@impcas.ac.cn</u> and ask for lpp-view issue solution.', alert_type='warning').servable(target='fileShow')
+            try:
+                console.log('Bokeh: loading...')
+                if len(checkbutton_display.value) == 2:
+                    show_Bokeh = Bokeh_show('new.lpp', 308., 3000, 4096, 0.1, 1.43, 0.2, 1.36, 0.5, 0.5)._show('Both')
+                elif len(checkbutton_display.value) == 1 and 'TOF' in checkbutton_display.value:
+                    show_Bokeh = Bokeh_show('new.lpp', 308., 3000, 4096, 0.1, 1.43, 0.2, 1.36, 0.5, 0.5)._show('TOF')
+                elif len(checkbutton_display.value) == 1 and 'Schottky' in checkbutton_display.value:
+                    show_Bokeh = Bokeh_show('new.lpp', 308., 3000, 4096, 0.1, 1.43, 0.2, 1.36, 0.5, 0.5)._show('Schottky')
+                else:
+                    pass
+                console.log('Bokeh: complete')
+                pn.pane.Alert('## Info\n### The current interface is for functional testing only. \n### Please wait for the final version.').servable(target='testShow')
+                pn.pane.Bokeh(show_Bokeh).servable(target='fileShow')
+                markdown_words.object = '## Upload and parse your .lpp file'
+                button_upload.disabled = False
+            except:
+                console.log('.lpp file error!')
+                markdown_words.object = '## Upload and parse your .lpp file'
+                pn.pane.Alert('## Alert\n### Some error exist when parsing your .lpp file.\nYou can send your .lpp file to <u>wangqian2016@impcas.ac.cn</u> and ask for lpp-view issue solution.', alert_type='warning').servable(target='fileShow')
         else:
             document.getElementById('testShow').innerHTML = ""
             display_info.object = '## Alert\n### Select display mode first!'
