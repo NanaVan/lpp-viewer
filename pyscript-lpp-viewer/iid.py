46694bea34cf7633ab4e3b7614b9736e4180df87
NanaVan <van4wang@hotmail.com>
2026-06-27 01:00:08 +0800

    change .db to .bin for cloudflare interference

--- pyscript-lpp-viewer/iid.py
+++ pyscript-lpp-viewer/iid.py
@@ -50,7 +50,7 @@ class IID():
         self.delta_v_over_v = delta_v_over_v # %
         self.L_CSRe = L_CSRe # m
         self.verbose = verbose
-        self.conn = sqlite3.connect("ionic_data.db")
+        self.conn = sqlite3.connect("ionic_data.bin")
         self.cur = self.conn.cursor()
 
         self.cur.execute("SELECT name FROM sqlite_master WHERE type='table'")
